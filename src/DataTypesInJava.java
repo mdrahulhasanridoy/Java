4f12ca1faf27e64d1175097458dc6bbf28c808fe
mdrahulhasanridoy <mdrahulhasanridoy@gmail.com>
2024-06-07 02:05:26 +0600

    Intellij Check

---
+++ src/DataTypesInJava.java
@@ -0,0 +1,20 @@
+public class DataTypesInJava {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = 10.535F;
+        System.out.println(price);
+        int number = 25;
+        System.out.println(number);
+        long big_number = 2134413566658678L;
+        System.out.println(big_number);
+        double big_price = 10.1241355;
+        System.out.println(big_price);
+        short n = 240;
+        System.out.println(n);
+    }
+}
---
+++ src/VariableInJava.java
@@ -0,0 +1,12 @@
+public class VariableInJava {
+    public static void main(String[] arga) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Rahul Hasan";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+    }
+}
