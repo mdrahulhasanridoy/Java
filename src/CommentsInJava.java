83dc9152e0d394703512b0ac5f4efcea557e99b5
mdrahulhasanridoy <mdrahulhasanridoy@gmail.com>
2024-06-07 19:08:17 +0600

    Comments In Java

---
+++ src/CommentsInJava.java
@@ -0,0 +1,10 @@
+public class CommentsInJava {
+    public static void main(String[] args) {
+        //SingleLine comment
+
+        /*
+        Multiline Comment
+        Share your message with us.
+         */
+    }
+}
