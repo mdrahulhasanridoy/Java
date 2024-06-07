c55ff9fc991e7f17b7bda3c7f6f691a46c974a6d
mdrahulhasanridoy <mdrahulhasanridoy@gmail.com>
2024-06-07 23:28:42 +0600

    type conversion (input by user)

---
+++ src/TypeConversionWithInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TypeConversionWithInput {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        float a = number.nextInt();
+        System.out.println(a);
+    }
+}
