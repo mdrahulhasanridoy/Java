594521b41109a69b3f5e2c98eefe67de02e9e818
mdrahulhasanridoy <mdrahulhasanridoy@gmail.com>
2024-06-07 19:08:51 +0600

    Product Of A And B

---
+++ src/ProductOfAB.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ProductOfAB {
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b =  sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
+    }
+}
