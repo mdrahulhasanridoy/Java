3948cd604f3657ef58ade817e2c39829be5b65b5
mdrahulhasanridoy <mdrahulhasanridoy@gmail.com>
2024-06-07 23:27:15 +0600

    char to int

---
+++ src/ASCIIvalue.java
@@ -0,0 +1,18 @@
+public class ASCIIvalue {
+    public static void main(String[] args) {
+        char ch1 = 'a';
+        char ch2 = 'z';
+        char ch3 = 'A';
+        char ch4 = 'Z';
+
+        int number1 = ch1;
+        int number2 = ch2;
+        int number3 = ch3;
+        int number4 = ch4;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+    }
+}
